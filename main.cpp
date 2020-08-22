4ed34181c45c32b24389f5425a4d9162dd4127e7
GuilhermeLLS <guiluizls@gmail.com>
2020-08-22 18:13:19 -0300

    add comments

--- main.cpp
+++ main.cpp
@@ -37,3 +37,49 @@ int main()
 
     return 0;
 }
+
+// symetric, asymetric anti-symetric
+
+// symetric [[1,2],[1,1],[1,3][3,1],[2,2],[1,1],[2,1]] => A = [1,2,3]
+// anti-symetric [[1,3][3,1],[2,2],[1,1],[2,1]] => A = [1,2,3]
+// asymetric [[1,3],[1,4],[2,1]] => A = [1,2,3,4]
+
+//! bool hasSimetricSimbling(int target[], int matrix[][])
+// {
+//     int m1 = matrix;
+//     if (target[0] == target[1])
+//     {
+//         return true;
+//     }
+
+//     bool found = false;
+//     for (int i = 0; i < m1.length; i++)
+//     {
+//         int pair = m1[i];
+//         if (pair != = target && pair[0] == = target[1] && pair[1] == = target[0])
+//         { // pair != target checks if it is happening a comparison to itself
+//             found = true;
+//             break;
+//         }
+//     }
+//     return found;
+// }
+
+// bool isSymetric(int matrix)
+// {
+//     int counter = 0;
+//     for (int i = 0; i < matrix.length; i++)
+//     {
+//         int pair = matrix[i];
+//         if (hasSimetricSimbling(pair, matrix))
+//         {
+//             counter = counter + 1;
+//         }
+//         else
+//         {
+//             break;
+//         }
+//     }
+//     return counter == matrix.length;
+// int myMatrix[10][2] = {{1, 2}, {1, 3}, {1, 4}, {1, 1}, {2, 2}, {2, 1}, {3, 1}, {4, 1}, {3, 3}, {4, 4}};
+//! }
